671bf02ad2784b66cd47c3bb2c64df5e04b40d4f
csunghoon7 <127923840+csunghoon7@users.noreply.github.com>
2023-08-03 14:15:54 +0900

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title('My parents New healthy Diner')
 
-print("hello streamlit")
+streamlit.header('아침식사 메뉴')
+streamlit.text('오메가 3 & 블루베리 오트밀')
+streamlit.text('케일, 시금치 & 로켓 스무디')
+streamlit.text('완숙 방목 계란')
+
