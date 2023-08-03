17eab3a9bff6544ab77be3190f152364e8f8872d
csunghoon7 <127923840+csunghoon7@users.noreply.github.com>
2023-08-03 14:17:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My parents New healthy Diner')
 
 streamlit.header('아침식사 메뉴')
-streamlit.text('오메가 3 & 블루베리 오트밀')
-streamlit.text('케일, 시금치 & 로켓 스무디')
-streamlit.text('완숙 방목 계란')
-
+streamlit.text('🥣오메가 3 & 블루베리 오트밀')
+streamlit.text('🥗케일, 시금치 & 로켓 스무디')
+streamlit.text('🐔완숙 방목 계란')
+streamlit.text('🥑🍞아보카도 토스트')
