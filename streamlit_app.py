7e6df254c63aa6c66bebf26ea414434caba1fcf5
csunghoon7 <127923840+csunghoon7@users.noreply.github.com>
2023-08-07 13:35:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
 
 streamlit.title('My parents New healthy Diner')
