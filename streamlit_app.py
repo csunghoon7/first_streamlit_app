b776a9af5d0d53d593d242db255b5bbf227b6e50
csunghoon7 <127923840+csunghoon7@users.noreply.github.com>
2023-08-09 10:50:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ except URLError as e:
 
 streamlit.title('My parents New healthy Diner')
 streamlit.header('아침식사 메뉴')
-streamlit.text('🥣오메가 3 & 블루베리 오트밀_최성훈')
+streamlit.text('🥣오메가 3 & 블루베리 오트밀')
 streamlit.text('🥗케일, 시금치 & 로켓 스무디')
 streamlit.text('🐔완숙 방목 계란') 
 streamlit.text('🥑🍞아보카도 토스트')
